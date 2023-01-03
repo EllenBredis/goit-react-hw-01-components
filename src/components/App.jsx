c0a6ac054796a210b2eabeed1ee84849b7c30b03
EllenBredis <ellen.od.ua@gmail.com>
2023-01-03 15:58:03 +0200

    hw-1

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import { Container } from './Container/Container';
+
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+
+import user from '../database/user.json';
+import data from '../database/data.json';
+import friends from '../database/friends.json';
+import transactions from '../database/transactions.json';
+
+
+export const App = () => {
+  return (
+    <Container>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </Container>
+  );
+};
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,31 @@
+// import PropTypes from 'prop-types';
+import style from './Statistics.module.css';
+
+ export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={style.statistics}>
+      {title && <h2 className={style.title}>{title}</h2>}
+
+      <ul className={style.list}>
+        {stats.map(state => (
+          <li className={style.item} key={state.id} >
+            <span className={style.label}>{state.label}</span>
+            <span className={style.percentage}>{state.percentage}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+  
+  // Statistics.propTypes = {
+  //   title: PropTypes.string,
+  //   stats: PropTypes.arrayOf(
+  //     PropTypes.exact({
+  //       id: PropTypes.string.isRequired,
+  //       label: PropTypes.string.isRequired,
+  //       percentage: PropTypes.number.isRequired,
+  //     })
+  //   ),
+  // };
